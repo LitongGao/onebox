688ac22cc7e9bb29c33528038222f103bfdc6fba
gaolitong <gao_litong1984@hotmail.com>
2016-09-13 17:46:22 +0800

    merge test4

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCleanup]
+        [DataSource("testxmlpath")]
+        
+        
+        public void TestMethod1()
+        {
+        }
+    }
+}
