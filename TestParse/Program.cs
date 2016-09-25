62e4d7b6871fece1e801a1382f9e8c6dd89f7d1d
gaolitong <gao_litong1984@hotmail.com>
2016-09-25 16:27:26 +0800

    update test

---
+++ TestParse/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(GetMgmtSystemEnumByName("123Pet"));
+            Console.WriteLine(DateTime.UtcNow.ToString("o"));
+            Console.WriteLine(DateTime.UtcNow.ToString());
+            Console.WriteLine(Environment.CurrentDirectory.ToString());
+            string a= Environment.CurrentDirectory.ToString();
+            a = Path.GetDirectoryName(a);
+            Console.WriteLine(a);
+            a = Path.GetDirectoryName(a);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+        public static SupportPMS GetMgmtSystemEnumByName(string sysName)
+        {
+            string displayName = sysName.Replace(' ', '_').Replace('/', '4')
+                .Replace('-', '5').Replace('.', '6');
+            Console.WriteLine("Displayname is: {0}", displayName);
+            int firstChar;
+            if (Int32.TryParse(displayName.Substring(0, 1), out firstChar))
+            {
+                return (SupportPMS)Enum.Parse(typeof(SupportPMS), "_" + displayName);
+            }
+            else
+            {
+                return (SupportPMS)Enum.Parse(typeof(SupportPMS), displayName);
+            }
+        }
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,19 +1,65 @@
-﻿using System;
+﻿using SocketClient;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestProject1
+namespace SocketClient.Tests
 {
-    [TestClass]
-    
+    [TestClass()]
+    [DeploymentItem("test2.xml")]
+    [DeploymentItem("test1.xml")]
     public class UnitTest1
     {
-        [TestMethod]
-        [TestCleanup]
-        [DataSource("testxmlpath")]
-        
-        
-        public void TestMethod1()
+        #region TextContext
+        private TestContext testContext;
+        public TestContext TestContext
         {
+            get { return testContext; }
+            set { testContext = value; }
+        }
+        #endregion
+        public string field1;
+        public string fields
+        {
+            get;
+            set;
+        }
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Console.WriteLine("ClassInitialize from here");
+            Console.ReadKey();
+        }
+
+        [TestInitialize]
+        public static void TestInitialize()
+        {
+            Console.WriteLine("TestInitialize from here");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Console.WriteLine("TestCleanup from here");
+        }
+        [ClassCleanup]
+        public void Classup()
+        {
+            Console.WriteLine("Class cleanup from here");
+        }
+
+        [TestMethod()]
+        [Owner("Peter")]
+        [Description("Verify test Send File")]
+        public void SendFileTest()
+        {
+            Assert.IsNull(field1);
+        }
+
+        [TestMethod()]
+        public void SendMessageTest()
+        {
+            Assert.Fail();
         }
     }
 }
+
